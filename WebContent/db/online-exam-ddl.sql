66a67c35eeb8ea8f82fc9c33cddc81ee3ed2b732
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-09 07:40:44 +0000

    joe updated

--- WebContent/db/online-exam-ddl.sql
+++ WebContent/db/online-exam-ddl.sql
@@ -1,10 +1,6 @@
 
     alter table EXAMS 
         drop 
-        foreign key FK3F55414AB2A5BEA;
-
-    alter table EXAMS 
-        drop 
         foreign key FK3F55414D8756695;
 
     alter table EXAMS 
@@ -13,7 +9,11 @@
 
     alter table EXAM_ANSWERS 
         drop 
-        foreign key FK65930C35CEA7EC76;
+        foreign key FK65930C35DE4D24AE;
+
+    alter table EXAM_ANSWERS 
+        drop 
+        foreign key FK65930C351ADB8C75;
 
     alter table EXAM_ANSWERS 
         drop 
@@ -23,13 +23,17 @@
         drop 
         foreign key FK3BDD512DD8756695;
 
-    alter table QUESTIONS 
-        drop 
-        foreign key FK3BDD512DCBB67AA3;
-
     alter table QUSTION_OPTION 
         drop 
         foreign key FK3FC9E1F16DC61D95;
+
+    alter table USER_CATEGORY 
+        drop 
+        foreign key FK48520EF260F8DCF5;
+
+    alter table USER_CATEGORY 
+        drop 
+        foreign key FK48520EF2D8756695;
 
     drop table if exists EXAMS;
 
@@ -43,6 +47,8 @@
 
     drop table if exists USERS;
 
+    drop table if exists USER_CATEGORY;
+
     create table EXAMS (
         EXAM_ID integer not null auto_increment,
         OBJ_VERSION integer,
@@ -52,17 +58,17 @@
         EXAM_SCORE integer,
         USER_ID integer not null,
         CATEGORY_ID integer not null,
-        exams_USER_ID integer,
         primary key (EXAM_ID)
     );
 
     create table EXAM_ANSWERS (
         ANSWER_ID integer not null auto_increment,
         OBJ_VERSION integer,
-        EXAM_ANSWER integer,
         IS_MARK_REVIEW bit,
         IS_ANSWERED bit,
+        EXAM_ANSWER integer not null,
         Question_ID integer not null,
+        EXAM_ID integer not null,
         primary key (ANSWER_ID)
     );
 
@@ -88,7 +94,6 @@
         IS_ESSAY bit not null,
         OPTION_ANSWER integer,
         CATEGORY_ID integer not null,
-        Questions_CATEGORY_ID integer,
         primary key (QUESTION_ID)
     );
 
@@ -119,11 +124,11 @@
         primary key (USER_ID)
     );
 
-    alter table EXAMS 
-        add index FK3F55414AB2A5BEA (exams_USER_ID), 
-        add constraint FK3F55414AB2A5BEA 
-        foreign key (exams_USER_ID) 
-        references USERS (USER_ID);
+    create table USER_CATEGORY (
+        USER_ID integer not null,
+        CATEGORY_ID integer not null,
+        primary key (USER_ID, CATEGORY_ID)
+    );
 
     alter table EXAMS 
         add index FK3F55414D8756695 (CATEGORY_ID), 
@@ -138,9 +143,15 @@
         references USERS (USER_ID);
 
     alter table EXAM_ANSWERS 
-        add index FK65930C35CEA7EC76 (ANSWER_ID), 
-        add constraint FK65930C35CEA7EC76 
-        foreign key (ANSWER_ID) 
+        add index FK65930C35DE4D24AE (EXAM_ANSWER), 
+        add constraint FK65930C35DE4D24AE 
+        foreign key (EXAM_ANSWER) 
+        references QUSTION_OPTION (OPTION_ID);
+
+    alter table EXAM_ANSWERS 
+        add index FK65930C351ADB8C75 (EXAM_ID), 
+        add constraint FK65930C351ADB8C75 
+        foreign key (EXAM_ID) 
         references EXAMS (EXAM_ID);
 
     alter table EXAM_ANSWERS 
@@ -155,14 +166,20 @@
         foreign key (CATEGORY_ID) 
         references EXAM_CATEGORYS (CATEGORY_ID);
 
-    alter table QUESTIONS 
-        add index FK3BDD512DCBB67AA3 (Questions_CATEGORY_ID), 
-        add constraint FK3BDD512DCBB67AA3 
-        foreign key (Questions_CATEGORY_ID) 
-        references EXAM_CATEGORYS (CATEGORY_ID);
-
     alter table QUSTION_OPTION 
         add index FK3FC9E1F16DC61D95 (QUESTION_ID), 
         add constraint FK3FC9E1F16DC61D95 
         foreign key (QUESTION_ID) 
         references QUESTIONS (QUESTION_ID);
+
+    alter table USER_CATEGORY 
+        add index FK48520EF260F8DCF5 (USER_ID), 
+        add constraint FK48520EF260F8DCF5 
+        foreign key (USER_ID) 
+        references USERS (USER_ID);
+
+    alter table USER_CATEGORY 
+        add index FK48520EF2D8756695 (CATEGORY_ID), 
+        add constraint FK48520EF2D8756695 
+        foreign key (CATEGORY_ID) 
+        references EXAM_CATEGORYS (CATEGORY_ID);
