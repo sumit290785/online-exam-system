a16e70bef2b3a1436e5bece1cbf48d1a2ffcc29a
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-09 06:35:56 +0000

    joe add

---
+++ trunk/src/com/onlineexam/service/impl/ExamServiceImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Software Engineering Course task
+ * copyright 2010 USS.
+ */
+package com.onlineexam.service.impl;
+
+import com.onlineexam.dao.ExamDAO;
+import com.onlineexam.domain.Exam;
+import com.onlineexam.domain.User;
+import com.onlineexam.service.ExamService;
+
+/**
+ * @author zhujoe
+ *
+ */
+public class ExamServiceImpl implements ExamService {
+	
+	private ExamDAO examDAO;
+	
+	
+
+	/* (non-Javadoc)
+	 * @see com.onlineexam.service.ExamService#caculateScore(com.onlineexam.domain.Exam)
+	 */
+	public boolean caculateScore(Exam exam) {
+		
+		return true;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.onlineexam.service.ExamService#createExam(com.onlineexam.domain.User)
+	 */
+	public Exam createExam(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ExamDAO getExamDAO() {
+		return examDAO;
+	}
+
+	public void setExamDAO(ExamDAO examDAO) {
+		this.examDAO = examDAO;
+	}
+	
+	
+
+}
