1ee3d863f10c203d2e8e74031ffe9b75db5e2147
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-18 18:50:21 +0000

    added by echo

---
+++ trunk/src/com/onlineexam/dao/impl/hibernate/CategoryDAOImpl.java
@@ -0,0 +1,12 @@
+package com.onlineexam.dao.impl.hibernate;
+
+import com.onlineexam.dao.CategoryDAO;
+import com.onlineexam.domain.Category;
+/**
+ * 
+ * @author chenecho
+ *
+ */
+public class CategoryDAOImpl extends GenericDAOImpl<Category> implements CategoryDAO{
+
+}
