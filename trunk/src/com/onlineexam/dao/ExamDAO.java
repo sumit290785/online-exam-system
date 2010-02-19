9c2764f66bbf525d746da4e1717c186b2bf45e91
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-19 16:55:58 +0000

    added By Echo

--- trunk/src/com/onlineexam/dao/ExamDAO.java
+++ trunk/src/com/onlineexam/dao/ExamDAO.java
@@ -4,12 +4,17 @@
  */
 package com.onlineexam.dao;
 
+import java.util.List;
+
+import com.onlineexam.domain.Answer;
+import com.onlineexam.domain.Category;
 import com.onlineexam.domain.Exam;
+import com.onlineexam.domain.User;
 
 /**
  * @author zhujoe
  *
  */
 public interface ExamDAO  extends GenericDAO<Exam>{
-
+	public Exam createExam(Category category,User user,List<Answer> answerLst);
 }
