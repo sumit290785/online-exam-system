2a9460e442d9d91939d277b88356fd7aa844df87
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-19 16:56:46 +0000

    commited by echo, changed by joe

--- src/com/onlineexam/domain/Answer.java
+++ src/com/onlineexam/domain/Answer.java
@@ -5,25 +5,28 @@
 package com.onlineexam.domain;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
 /**
  * @author Joe Zhu
- *
+ * 
  */
 
 @Entity
 @Table(name = "EXAM_ANSWERS")
-public class Answer implements Serializable{
+public class Answer implements Serializable {
 	// common attributes for all POJO
 	@Id
 	@GeneratedValue
@@ -33,36 +36,40 @@ public class Answer implements Serializable{
 	@Version
 	@Column(name = "OBJ_VERSION")
 	private int version = 0;
-	
-	//other attributes
-	
-	@ManyToOne
+
+	// other attributes
+
+	@OneToOne
 	@JoinColumn(name = "Question_ID", nullable = false)
 	private Question question;
-	
-	@OneToOne
-	@JoinColumn(name = "EXAM_ANSWER", nullable = false)
-	private Option answer; 
-	
-	//if the question is once every page
+
+	@ManyToMany
+	@JoinTable(name = "ANSWER_OPTION", joinColumns = { @JoinColumn(name = "ANSWER_ID") }, inverseJoinColumns = { @JoinColumn(name = "OPTION_ID") })
+	private Set<Option> answers = new HashSet<Option>();
+
+	// if the question is once every page
 	@Column(name = "IS_MARK_REVIEW", nullable = true)
 	private boolean markReview;
-	
+
 	@Column(name = "IS_ANSWERED", nullable = true)
 	private boolean answered;
 	
+	@Column(name = "SEQUENCE_ID")
+	private int sequenceId;
+
 	// ********************** Constructor ********************** //
-	
+
 	public Answer(Question question) {
 		super();
 		this.question = question;
 	}
-	public Answer(){}
-	
+
+	public Answer() {
+	}
+
 	public int getVersion() {
 		return version;
 	}
-
 
 	public int getId() {
 		return id;
@@ -74,14 +81,6 @@ public class Answer implements Serializable{
 
 	public void setQuestion(Question question) {
 		this.question = question;
-	}
-
-	public Option getAnswer() {
-		return answer;
-	}
-
-	public void setAnswer(Option answer) {
-		this.answer = answer;
 	}
 
 	public boolean isMarkReview() {
@@ -99,5 +98,22 @@ public class Answer implements Serializable{
 	public void setAnswered(boolean answered) {
 		this.answered = answered;
 	}
+
+	public Set<Option> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(Set<Option> answers) {
+		this.answers = answers;
+	}
+
+	public int getSequenceId() {
+		return sequenceId;
+	}
+
+	public void setSequenceId(int sequenceId) {
+		this.sequenceId = sequenceId;
+	}
+
 	
 }
