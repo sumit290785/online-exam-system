b20e149b8919c3d3f829209adb69b06b651b62d4
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-19 16:56:22 +0000

    added By Echo

--- trunk/src/com/onlineexam/dao/impl/hibernate/OptionDAOImpl.java
+++ trunk/src/com/onlineexam/dao/impl/hibernate/OptionDAOImpl.java
@@ -1,7 +1,12 @@
 package com.onlineexam.dao.impl.hibernate;
 
+import java.util.List;
+
+import org.hibernate.criterion.DetachedCriteria;
+
 import com.onlineexam.dao.OptionDAO;
 import com.onlineexam.domain.Option;
+import com.onlineexam.domain.Question;
 
 /**
  * 
@@ -10,5 +15,6 @@ import com.onlineexam.domain.Option;
  */
 
 public class OptionDAOImpl extends GenericDAOImpl<Option> implements OptionDAO {
+
 
 }
--- trunk/src/com/onlineexam/dao/impl/hibernate/QuestionDAOImpl.java
+++ trunk/src/com/onlineexam/dao/impl/hibernate/QuestionDAOImpl.java
@@ -1,7 +1,18 @@
 package com.onlineexam.dao.impl.hibernate;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+
+
 import com.onlineexam.dao.QuestionDAO;
+import com.onlineexam.domain.Category;
+import com.onlineexam.domain.Option;
 import com.onlineexam.domain.Question;
 
 /**
@@ -11,4 +22,29 @@ import com.onlineexam.domain.Question;
  */
 public class QuestionDAOImpl extends GenericDAOImpl<Question> implements QuestionDAO {
 
+	@SuppressWarnings("unchecked")
+	public List<Question> getRandomeQuestions(Category category) {
+		// TODO Auto-generated method stub
+		EntityTransaction tx = this.getEntityManager().getTransaction();
+		tx.begin();
+		List<Question> result = getEntityManager().createQuery(
+				"from " + getEntityBeanType().getName()+" where question.category.id ="+category.getId()+" order by rand").setMaxResults(category.getTotalQuestions()).getResultList();
+		
+		tx.commit();
+		return result;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Option> getCorrectOptions(Question question) {
+		// TODO Auto-generated method stub
+//		Option option = new Option();
+//		option.setCorrect(true);
+		List<Option> optionLst = new ArrayList(question.getOptions());
+		List<Option> correctLst = new ArrayList();
+		for (int i=0;i<optionLst.size();i++){
+			Option option = optionLst.get(i);
+			if (option.isCorrect()){correctLst.add(option);}			
+		}
+		return correctLst;
+	}
 }
