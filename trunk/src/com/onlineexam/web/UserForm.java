83202b36a5a63375966b7db770de6701356fdc3c
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-24 13:18:21 +0000

    Joe Added

---
+++ trunk/src/com/onlineexam/web/UserForm.java
@@ -0,0 +1,116 @@
+package com.onlineexam.web;
+
+import antlr.collections.List;
+
+public class UserForm {
+	
+	private String userId;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private String street;
+	private String zipcode;
+	private String city;
+	private String userType;
+	private List Category;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public List getCategory() {
+		return Category;
+	}
+
+	public void setCategory(List category) {
+		Category = category;
+	}
+
+}
