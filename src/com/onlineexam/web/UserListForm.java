d38d021f64b743668bd6a0bdf241d02d9f458ec5
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-24 13:18:21 +0000

    Joe Added

---
+++ src/com/onlineexam/web/UserListForm.java
@@ -0,0 +1,20 @@
+package com.onlineexam.web;
+
+import java.util.List;
+
+import com.onlineexam.domain.User;
+
+public class UserListForm {
+	private List<User> users;
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
+	
+
+}
