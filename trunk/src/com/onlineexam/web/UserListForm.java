48cc13d5cf6b825c238c6c650c1868d4b1fdf08f
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-25 13:34:28 +0000

    joe updated

--- trunk/src/com/onlineexam/web/UserListForm.java
+++ trunk/src/com/onlineexam/web/UserListForm.java
@@ -3,9 +3,13 @@ package com.onlineexam.web;
 import java.util.List;
 
 import com.onlineexam.domain.User;
+import com.onlineexam.main.ServiceHandler;
+import com.onlineexam.service.AccountService;
 
 public class UserListForm {
-	private List<User> users;
+
+	private List<User> users = ((AccountService) ServiceHandler.getInstance()
+			.getService("accountService")).getAllUsers();
 
 	public List<User> getUsers() {
 		return users;
@@ -14,7 +18,5 @@ public class UserListForm {
 	public void setUsers(List<User> users) {
 		this.users = users;
 	}
-	
-	
 
 }
