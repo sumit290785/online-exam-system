0a0c9ead499fa2394e39655202701695d93f1eb8
echo3010 <echo3010@e045c2c6-0a51-11df-bd4f-1fc3cb39d6f8>
2010-02-26 07:49:14 +0000

    joe updated

--- src/com/onlineexam/web/UserListForm.java
+++ src/com/onlineexam/web/UserListForm.java
@@ -23,21 +23,63 @@ public class UserListForm {
 			user.setEmail(u.getEmail());
 			user.setPhone(u.getPhone());
 			user.setStreet(u.getStreet());
-			user.setUserId("" + u.getId());
+			user.setUserId(u.getId());
 			switch (u.getUserType()) {
 			case ADMIN:
-				user.setUserType("ADMIN");
+				user.setUserType("admin");
+				break;
 			case TEACHER:
-				user.setUserType("TEACHER");
+				user.setUserType("teacher");
+				break;
 			case STUDENT:
-				user.setUserType("STUDENT");
+				user.setUserType("student");
+				break;
 			}
 			users.add(user);
 		}
 
 	}
 
+	public String searchUserByName() {
+		users = new ArrayList<UserForm>();
+		User u;
+		try {
+			u = ((AccountService) ServiceHandler.getInstance().getService(
+					"accountService")).getUser(searchUser);
+			searchUser = "";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "userList";
+		}
+
+		UserForm user = new UserForm();
+		user.setUsername(u.getUsername());
+		user.setPassword(u.getPassword());
+		user.setFirstName(u.getFirstName());
+		user.setLastName(u.getLastName());
+		user.setCity(u.getCity());
+		user.setEmail(u.getEmail());
+		user.setPhone(u.getPhone());
+		user.setStreet(u.getStreet());
+		user.setUserId(u.getId());
+		switch (u.getUserType()) {
+		case ADMIN:
+			user.setUserType("admin");
+			break;
+		case TEACHER:
+			user.setUserType("teacher");
+			break;
+		case STUDENT:
+			user.setUserType("student");
+			break;
+		}
+		users.add(user);
+		return "userList";
+	}
+
 	private List<UserForm> users;
+
+	private String searchUser;
 
 	public List<UserForm> getUsers() {
 		return users;
@@ -45,6 +87,14 @@ public class UserListForm {
 
 	public void setUsers(List<UserForm> users) {
 		this.users = users;
+	}
+
+	public String getSearchUser() {
+		return searchUser;
+	}
+
+	public void setSearchUser(String searchUser) {
+		this.searchUser = searchUser;
 	}
 
 }
